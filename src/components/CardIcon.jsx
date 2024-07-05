989ac516a99d10ba5561d6d0fc094792d5cdbe8f
Yağız Can Dinç <ycandiinc@gmail.com>
2024-07-05 09:40:19 +0300

    GlobalStateManagemet ContexAPI kullanımı yapıldı

--- src/components/CardIcon.jsx
+++ src/components/CardIcon.jsx
@@ -1,11 +1,18 @@
 import { FiShoppingCart } from "react-icons/fi";
+import { MyContext } from "../main";
+import { useContext } from "react";
 
 const CardIcon = ({ cart }) => {
+  const { myState, setMyState } = useContext(MyContext);
   return (
     <>
       {/*absolute eksenden çıkmasını sağladı top-2 en tepeye çıkardı relative ise kapsayıcısı olan div e hapsolmasını sağladı*/}
       <div className="relative">
-        <FiShoppingCart className="text-2xl" />
+        <FiShoppingCart
+          className="text-2xl"
+          onClick={() => setMyState("Yağız")}
+        />
+        {myState}
         {cart.length > 0 && (
           <span className="bg-red-500 text-white flex w-5 h-5 justify-center items-center rounded-full absolute -top-2 -right-4 text-xs">
             {cart.length}
